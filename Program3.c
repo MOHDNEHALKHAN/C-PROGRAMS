17b49b24aceba0e0ded0b0f8bbf4afbe00a31864
MOHD NEHAL KHAN <nehalk482@gmail.com>
2023-08-17 10:36:55 +0530

    Added 2 more programs

---
+++ Program3.c
@@ -0,0 +1,16 @@
+/*PROGRAM USING ARITHMETICAL OPERATORS*/
+
+#include<stdio.h>
+int main()
+{
+int a = 9, b = 4, c;
+c = a+b;
+printf("a+b = %d \n",c);
+c = a-b;
+printf("a-b = %d \n",c);
+c = a*b;
+printf("a*b = %d \n",c);
+c = a%b;
+printf("remainder = %d \n",c);
+return 0;
+}
---
+++ Program4.c
@@ -0,0 +1,12 @@
+/*PROGRAM USING RELATIONAL OPERATORS*/
+
+#include<stdio.h>
+int main()
+{
+int a=5, b=5 , c =10;
+printf("%d\n",a==b);
+printf("%d\n",a==c);
+printf("%d\n",a>b);
+printf("%d\n",a<c);
+return 0;
+}
