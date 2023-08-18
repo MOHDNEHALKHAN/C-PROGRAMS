c94c5441e97e250d1397268843e823241782b1dc
MOHD NEHAL KHAN <nehalk482@gmail.com>
2023-08-18 09:27:44 +0530

    Added products related to logical , assignment & bitwise Operators

---
+++ Program7.c
@@ -0,0 +1,9 @@
+/*PROGRAM USING BITWISE OPERATORS*/
+
+#include<stdio.h>
+int main()
+{
+int a=6 , b=4;  // variablr declaration
+printf("The output of the bitwise AND operator is %d",a&b);
+return 0;
+}
