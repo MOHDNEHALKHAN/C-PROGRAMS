2c76688db7f9819efbcdb0c864507a4c0b7fe1a5
MOHD NEHAL KHAN <nehalk482@gmail.com>
2023-08-18 09:37:52 +0530

    Revert "Added products related to logical , assignment & bitwise Operators"
    This reverts commit c94c5441e97e250d1397268843e823241782b1dc.

--- Program7.c
+++
@@ -1,9 +0,0 @@
-/*PROGRAM USING BITWISE OPERATORS*/
-
-#include<stdio.h>
-int main()
-{
-int a=6 , b=4;  // variablr declaration
-printf("The output of the bitwise AND operator is %d",a&b);
-return 0;
-}
