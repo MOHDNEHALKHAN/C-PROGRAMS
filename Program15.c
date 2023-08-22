0223299d3c80c3052cc498e640812c14377468af
MOHD NEHAL KHAN <nehalk482@gmail.com>
2023-08-22 08:52:12 +0530

    Add 2 more programs

---
+++ Program15.c
@@ -0,0 +1,13 @@
+/*PROGRAM USING GET & PUT FUNCTION*/
+
+#include<stdio.h>
+int main()
+{
+char a[50];
+printf("enter your name");
+
+gets(a);
+printf("Your name is");
+puts(a);
+return 0;
+}
---
+++ Program16.c
@@ -0,0 +1,10 @@
+/*WRITE A C PROGRAM - USING GETS() AND PUTS() DO DISPLAY THE INPUT CHARACTER*/
+
+#include<stdio.h>
+int main()
+{
+char ch[100];    //Character array with size 100
+gets(ch);
+puts(ch);
+return 0;
+}
