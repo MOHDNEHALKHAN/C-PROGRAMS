a7f21943e6af88870864645ed4b802fbfa594d98
MOHD NEHAL KHAN <nehalk482@gmail.com>
2023-08-27 12:56:14 +0530

    Added do while & for loop

---
+++ Program26.c
@@ -0,0 +1,14 @@
+/*PROGRAM USING DO WHILE LOOP*/
+
+#include<stdio.h>
+int main()
+{
+int num=1; //initialization the variable
+do
+{
+printf("%d\n",num);
+num++;   // incrementing operation
+}
+while(num<=10);
+return 0;
+}
---
+++ Program27.c
@@ -0,0 +1,12 @@
+/*PROGRAM USING FOR LOOP*/
+
+#include<stdio.h>
+int main()
+{
+int num;
+for(num =0;num<=10;num++)
+{
+printf("%d\n",num);
+} 
+return 0;
+}
