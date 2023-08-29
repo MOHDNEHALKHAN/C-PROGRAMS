47f7f81732a63740950d180061bb28930eca9e92
MOHD NEHAL KHAN <nehalk482@gmail.com>
2023-08-29 11:40:19 +0530

    Add pre-userdefined functions prog

---
+++ Program31.c
@@ -0,0 +1,13 @@
+/*PROGRAM USING PREDEFINE FUNCTION*/
+
+#include<stdio.h>
+void hello(); // Declaration
+
+int main()
+{
+hello(); // Function call
+}
+void hello() // Definition
+{
+printf("Welcome to the world of c programming\n");
+}
---
+++ Program32.c
@@ -0,0 +1,13 @@
+/*PROGRAM USING USERDEFINE FUNCTION*/
+
+#include<stdio.h>
+void cow(); // Declaration or prototype
+
+int main()
+{
+cow(); // Function call
+}
+void cow() // Definition
+{
+printf("I am a domestic animal\n");
+}
