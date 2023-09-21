114ac9fac0c50c22c9170dacdc20d7a46e4b73da
MOHD NEHAL KHAN <nehalk482@gmail.com>
2023-09-21 10:49:39 +0530

    added calloc program

---
+++ Program91.c
@@ -0,0 +1,35 @@
+/*PROGRAM USING ALLOCATING MEMORY WITH CALLOC*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{ 
+    int *ptr;
+    int n, i; // Get the number of elements
+
+printf("Enter number of elements:\n"); 
+scanf("%d",&n);
+//Dynamically allocate memory using malloc()
+ptr= (int *)calloc(n, sizeof(int));
+if (ptr == NULL) {
+printf("Heap is full\n");
+exit(0);
+}
+else {
+// Memory has been successfully allocated
+printf("Memory successfully  allocated  using calloc.\n");
+// Get the elements
+for (i=0; i<n; ++i)
+{
+scanf("%d", (ptr+i)); 
+} 
+// Print the elements of the array
+printf("The elements are:\n"); 
+for (i=0; i<n;i++)
+  {
+printf("%d\n", (ptr+i));
+  }
+ }
+return 0;
+}
